c374810b6c4e6f5be8616a3ba264a806e64ebbce
Nafanael Pereira Alves <126331218+nafanael@users.noreply.github.com>
2024-05-07 19:34:28 -0300

    remoção do css das paginas php

--- core/views/criarProva.php
+++ core/views/criarProva.php
@@ -6,67 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= APP_NAME ?> | Criação de prova</title>
     <link rel="stylesheet" href="./public/assets/css/criarProvas.css">
-    <!-- <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #ededed;
-            margin: 0;
-            padding: 0;
-        }
-
-        .entrada {
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 50px;
-            background-color: #fff;
-            border-radius: 5px;
-            margin-top: 50px;
-            box-shadow: 3px 5px 10px rgba(0, 0, 0, 0.3);
-
-        }
-
-
-        h2 {
-            text-align: center;
-            color: #ff6f00;
-        }
-
-        label {
-            display: block;
-            font-weight: bold;
-            margin-bottom: 10px;
-            color: #333;
-        }
-
-        /* Estilizando o botão de envio */
-        input[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            margin-top: 20px;
-            background-color: #ff6f00;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 20px;
-        }
-
-        /* Estilizando as seleções de período */
-        select {
-            width: 48%;
-            padding: 8px;
-            border: 1px solid #ff6f00;
-            border-radius: 5px;
-            font-size: 16px;
-        }
-
-        h1 {
-            color: #ff6f00;
-        }
-    </style> -->
 
 </head>
-
 <body>
     <?php 
     $gestor = new PDO("mysql:host=" . 'localhost' . ";dbname=" . 'automind' . ";charset=utf8", 'root', '');
--- core/views/log.php
+++ core/views/log.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entre para nossa familia</title>
+    <link rel="stylesheet" href="./public/assets/css/log.css">
 </head>
 <body>
     <a href="./?a=login">Login</a>
