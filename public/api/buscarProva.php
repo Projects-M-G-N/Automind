b422e6669fab27f308392985782302ddfed93438
Gabriel Kleber <gabrielkleber54321n@gmail.com>
2024-06-24 18:08:18 -0300

    Só falta adicionar a questão em si
    Essa vai ser a parte mais fácil, eu acho

---
+++ public/api/buscarProva.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+$id_prova = filter_input(INPUT_GET, 'idProva', FILTER_DEFAULT);
+
+$gestor = new PDO("mysql:host=" . 'localhost' . ";dbname=" . 'automind' . ";charset=utf8", 'root', '');
+
+$numeroQuest = $gestor->query("SELECT COUNT(id) as quant FROM questao WHERE idprova='$id_prova'")->fetch()['quant'];
+$resultados = $gestor->query("SELECT * FROM questao WHERE idprova='$id_prova'");
+
+$listaResult = array();
+array_push($listaResult, $numeroQuest);
+
+$listaQuest = array();
+$listaAlt = array();
+while($resultado = $resultados->fetch(PDO::FETCH_ASSOC)) {
+    array_push($listaQuest, $resultado);
+    $id = $resultado['id'];
+    $alt = $gestor->query("SELECT * FROM alternativas WHERE id='$id'");
+    while($alter = $alt->fetch(PDO::FETCH_ASSOC)) {
+        array_push($listaAlt, $alter);
+    }
+}
+
+array_push($listaResult, $listaQuest);
+array_push($listaResult, $listaAlt);
+
+echo json_encode($listaResult);
