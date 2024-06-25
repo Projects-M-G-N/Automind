a2f86c9913a98ef7847fe410e954459ea03685fd
Gabriel Kleber <gabrielkleber54321n@gmail.com>
2024-06-25 17:06:11 -0300

    Eu acho que é isso por agora

--- core/views/src/prova.php
+++ core/views/src/prova.php
@@ -5,24 +5,7 @@
             
         </div>
         <div class="questoes">
-            <div class="questao">
-                <div class="texto">
-                    <p>1) Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis recusandae, iure expedita, maiores natus sit explicabo quis eaque autem sunt asperiores eligendi maxime voluptatum saepe totam. Quod quae ipsam harum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam alias, maiores eaque autem id aliquam qui, quo accusamus minima, sint quis animi deleniti aspernatur quas doloremque ab corporis delectus. Aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolor minus iure cumque atque ullam ea, fugit unde vitae rem laudantium tempora odit magnam qui quod repellat at officia magni.</p>
-                </div>
-                <div class="img">
-
-                </div>
-                <div class="pergunta">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat libero, commodi porro totam perferendis at odio alias earum quo, delectus rerum amet exercitationem hic ratione quibusdam quisquam illo eligendi dolorem.</p>
-                </div>
-                <div class="alt">
-                    <p>a- enbfyefywbfuyefuygerfgu</p>
-                    <p>b- foweuihuirhyrehyeg</p>
-                    <p>c- fkorniuerhfyureufy</p>
-                    <p>d- fiorefuerhfwjfuihfuirh</p>
-                    <p>e- fhuerfhyierhyirfghyerifgyu</p>
-                </div>
-            </div>
+            
         </div>
     </div>
     <div class="opcoes">
